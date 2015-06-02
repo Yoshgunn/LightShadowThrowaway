106cad52552686ae2f17b8d5ec261a7ec5d529b4
Jacquies <j4cqu13s@gmail.com>
2015-06-01 20:01:26 -0400

    Spotlight Level
    Added a level to showcase the spotlight. It's very simple, with little
    interactivity.
    
    Later I'll do one that shows how spotlights interact with other
    mechanics, but that is out of the scope of this task.

---
+++ ThrowawayProject/Assets/_Scripts/Spotlight.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spotlight : MonoBehaviour, Triggerable {
+	private bool isTriggered = false;
+
+	// Use this for initialization
+	void Start () {
+		if (!isTriggered) {
+			this.GetComponent<Light>().enabled = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Triggerable.Trigger(){
+		this.GetComponent<Light>().enabled = !this.GetComponent<Light>().enabled;
+	}
+
+	void Triggerable.UnTrigger(){
+		this.GetComponent<Light>().enabled = !this.GetComponent<Light>().enabled;
+	}
+}
