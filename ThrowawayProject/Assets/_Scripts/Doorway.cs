016970fa4952f17df37b820dc24983569eeeb151
Jacquies <j4cqu13s@gmail.com>
2015-07-22 18:47:08 -0400

    Doorway Connection
    A set of doorways now consists of two separate 'doorway' objects, each
    of which should have a reference to the other.
    
    See GitHub issue #10.

--- ThrowawayProject/Assets/_Scripts/Doorway.cs
+++ ThrowawayProject/Assets/_Scripts/Doorway.cs
@@ -3,26 +3,30 @@ using System.Collections;
 
 public class Doorway : MonoBehaviour {
 
-	GameObject doorway1;
-	Node doorwayNode1;
-	GameObject doorway2;
-	Node doorwayNode2;
+	public Doorway connectedTo;
+
+	//GameObject doorway1;
+	//Node doorwayNode1;
+	//GameObject doorway2;
+	//Node doorwayNode2;
+	Node myNode;
 	bool transitioning = false;
 	int counter;
 	bool onDoorOneSide = true;
 
 	// Use this for initialization
 	void Start () {
-		doorway1 = this.transform.GetChild (0).gameObject;
-		doorwayNode1 = doorway1.GetComponentInChildren<Node> ();
-		doorway2 = this.transform.GetChild (1).gameObject;
-		doorwayNode2 = doorway2.GetComponentInChildren<Node> ();
+		//doorway1 = this.transform.GetChild (0).gameObject;
+		//doorwayNode1 = doorway1.GetComponentInChildren<Node> ();
+		//doorway2 = this.transform.GetChild (1).gameObject;
+		//doorwayNode2 = doorway2.GetComponentInChildren<Node> ();
+		myNode = this.transform.GetComponentInChildren<Node> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		//If we're on one of the doors, fade the light out, then move, then fade it in.
-		if (!transitioning) {
+		/*if (!transitioning) {
 			if ((PathfindingPlayer.PLAYER.GetCurrentNode () == doorwayNode1 || PathfindingPlayer.PLAYER.GetCurrentNode () == doorwayNode2) && PathfindingPlayer.PLAYER.GetTargetNode () == null) {
 				//We're in a doorway
 				transitioning = true;
@@ -49,6 +53,42 @@ public class Doorway : MonoBehaviour {
 					Node.currentNode = doorwayNode1;
 				}
 			}
+		}*/
+
+		//If we're on one of the doors, fade the light out, then move, then fade it in.
+		if (!transitioning) {
+			if (PathfindingPlayer.PLAYER.GetCurrentNode() == myNode && PathfindingPlayer.PLAYER.GetTargetNode() == null){
+				//We're in this doorway - make sure all of the conditions are met:
+				//There is a door to connect to
+				//There is a node on the other side of that door
+				if (connectedTo && connectedTo.GetComponentInChildren<Boundary>() && connectedTo.GetComponentInChildren<Boundary>().GetConnectedTo ()){
+					transitioning = true;
+					counter = Application.targetFrameRate/2;
+					PathfindingPlayer.PLAYER.GetComponentInChildren<MyLight>().FadeOut(Application.targetFrameRate/2);
+				}
+			}
+		} else {
+			if (counter > 0){
+				//We're still waiting to move over
+				counter--;
+			}else{
+				//And now we're actually moving
+				transitioning = false;
+				PathfindingPlayer.PLAYER.GetComponentInChildren<MyLight>().FadeIn(Application.targetFrameRate);
+				PathfindingPlayer.PLAYER.SetTargetNode(connectedTo.GetComponentInChildren<Boundary>().GetConnectedTo().GetNode ());
+				PathfindingPlayer.PLAYER.transform.position = connectedTo.transform.position;
+				PathfindingPlayer.PLAYER.SetCurrentNode(connectedTo.GetNode ());
+				Node.currentNode = connectedTo.GetNode ();
+			}
 		}
+	}
+
+	public Node GetNode(){
+		return myNode;
+	}
+
+	public static void ConnectDoorways(Doorway d1, Doorway d2){
+		d1.connectedTo = d2;
+		d2.connectedTo = d1;
 	}
 }
