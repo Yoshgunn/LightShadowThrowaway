2c5a400fb142dd5148c6ccfb80b8e0fb80ce29c1
Jacquies <j4cqu13s@gmail.com>
2015-11-02 18:01:09 -0500

    Tile Prefabs Striping Fix
    Added shadow object to tile prefabs.

--- ThrowawayProject/Assets/_Scripts/Editor/AwesomeEditorWindow.cs
+++ ThrowawayProject/Assets/_Scripts/Editor/AwesomeEditorWindow.cs
@@ -21,13 +21,35 @@ public class AwesomeEditorWindow : EditorWindow {
 			foreach (GameObject obj in objList){
 				if (obj is GameObject){
 					temp = (GameObject)obj;
-					if (temp.hideFlags==HideFlags.None && temp.GetComponent<Renderer>() && temp.GetComponent<Renderer>().enabled){
-						Debug.Log (obj);
-						GameObject go = Object.Instantiate (temp, temp.transform.position, temp.transform.rotation) as GameObject;
-						go.transform.localScale = go.transform.localScale * 0.99f;
-						go.GetComponent<Renderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
-						//go.transform.SetParent (temp.transform.parent);
-						temp.GetComponent<Renderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+
+					//Is the object 'hidden'?
+					if (temp.hideFlags==HideFlags.None){
+						//Does it have a renderer enabled?
+						if (temp.GetComponent<Renderer>() && temp.GetComponent<Renderer>().enabled){
+							//Is it a cube that's not a prefab?
+							//if ((PrefabUtility.GetPrefabType(temp)==PrefabType.None || PrefabUtility.GetPrefabType(temp)==PrefabType.PrefabInstance) && temp.GetComponent<MeshFilter>() && temp.GetComponent<MeshFilter>().mesh && (temp.GetComponent<MeshFilter>().mesh.name=="Cube Instance" || temp.GetComponent<MeshFilter>().mesh.name=="Cube")){
+							if (temp.GetComponent<MeshFilter>() && ((PrefabUtility.GetPrefabType(temp)==PrefabType.None && temp.GetComponent<MeshFilter>().mesh && temp.GetComponent<MeshFilter>().mesh.name=="Cube") || (PrefabUtility.GetPrefabType(temp)==PrefabType.PrefabInstance && temp.GetComponent<MeshFilter>().sharedMesh && temp.GetComponent<MeshFilter>().sharedMesh.name=="Cube"))){
+								Debug.Log (temp);
+								GameObject newParent = Object.Instantiate (new GameObject(), temp.transform.position, temp.transform.rotation) as GameObject;
+								newParent.transform.SetParent (temp.transform.parent);
+								newParent.name = temp.name + " Parent";
+								temp.transform.SetParent (newParent.transform);
+
+								/*GameObject go = Object.Instantiate (temp, temp.transform.position, temp.transform.rotation) as GameObject;
+								go.transform.localScale = go.transform.localScale * 0.99f;
+								go.GetComponent<Renderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
+								//go.transform.SetParent (temp.transform.parent);
+								temp.GetComponent<Renderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;*/
+							}
+
+							if (PrefabUtility.GetPrefabType(temp)!=PrefabType.None && temp.GetComponent<MeshFilter>() && temp.GetComponent<MeshFilter>().sharedMesh && (temp.GetComponent<MeshFilter>().sharedMesh.name=="Cube Instance" || temp.GetComponent<MeshFilter>().sharedMesh.name=="Cube")){
+								//Debug.Log (temp + " is a prefab of type " + PrefabUtility.GetPrefabType(temp) + ", " + temp.GetInstanceID());
+								/*GameObject newParent = Object.Instantiate (new GameObject(), temp.transform.position, temp.transform.rotation) as GameObject;
+								newParent.transform.SetParent (temp.transform.parent);
+								newParent.name = temp.name + " Parent";
+								temp.transform.SetParent (newParent.transform);*/
+							}
+						}
 					}
 				}
 			}
