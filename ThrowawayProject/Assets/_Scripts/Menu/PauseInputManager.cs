a778548475a8ebf24777d122b188046a865cd52a
Yoshgunn <yoshgunn@gmail.com>
2015-11-07 02:39:22 -0500

    [UI] Pause Menu
    I made the pause menu and then thought of a great way we can do menus
    where we don't need this at all. Woo!

---
+++ ThrowawayProject/Assets/_Scripts/Menu/PauseInputManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseInputManager : MonoBehaviour 
+{
+	public GameObject PauseMenu;
+
+	public void PauseMenuToggle ()
+	{
+		PauseMenu.SetActive ( !PauseMenu.activeSelf );
+	}
+}
